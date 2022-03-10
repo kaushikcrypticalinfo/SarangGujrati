3aee07e006322d5e9a4673f9e2d0d7ccdf5a8a69
kaushik.crypticalinfo@gmail.com <Kaushik@123Cryptical>
2022-03-10 12:42:17 +0530

    - Resolve youtube player issue.

--- app/src/main/java/com/example/saranggujrati/ui/activity/YouTubeActivity.kt
+++ app/src/main/java/com/example/saranggujrati/ui/activity/YouTubeActivity.kt
@@ -39,7 +39,7 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 
-class YouTubeActivity: YouTubeBaseActivity() {
+class YouTubeActivity : YouTubeBaseActivity() {
 
     private val TAG = "YoutubeActivity"
     private lateinit var youTubePlayer: YouTubePlayer
@@ -48,60 +48,56 @@ class YouTubeActivity: YouTubeBaseActivity() {
     var urlLink: String? = null
     var videoId: String? = null
 
-     private lateinit var mActivity: MainActivity
+    private lateinit var mActivity: MainActivity
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityYoutubeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mActivity= MainActivity()
+        mActivity = MainActivity()
 
         loadBannerAd()
 
 
-        binding.toolbar.tvTitle.text=getString(R.string.back_to_channel)
-        binding.toolbar.icBack.setOnClickListener {
+        binding.tvTitle.text = getString(R.string.back_to_channel)
+        binding.icBack.setOnClickListener {
             onBackPressed()
         }
 
-        binding.toolbar.tvTitle.setOnClickListener {
+        binding.tvTitle.setOnClickListener {
             onBackPressed()
         }
 
-        if(intent.getStringExtra("url")!=null){
-            urlLink=intent.getStringExtra("url")
+        if (intent.getStringExtra("url") != null) {
+            urlLink = intent.getStringExtra("url")
 
         }
         getYouTubeId(urlLink!!)
-        if(videoId!=null){
+        if (videoId != null) {
             playVideo()
 
-        }else{
-            Toast.makeText(AppClass.appContext , "Error" , Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(AppClass.appContext, "Error", Toast.LENGTH_SHORT).show()
 
         }
 
     }
 
 
-    private fun loadBannerAd(){
+    private fun loadBannerAd() {
         val adRequest = AdRequest.Builder().build()
         binding.adView.loadAd(adRequest)
 
-        binding.adView.adListener  = object : AdListener(){
+        binding.adView.adListener = object : AdListener() {
             override fun onAdFailedToLoad(@NonNull p0: LoadAdError) {
                 super.onAdFailedToLoad(p0)
             }
-
         }
-
-
-
     }
 
     override fun onPause() {
-        if (binding.adView!=null) {
+        if (binding.adView != null) {
             binding.adView!!.pause();
         }
         super.onPause()
@@ -120,12 +116,13 @@ class YouTubeActivity: YouTubeBaseActivity() {
         }
         super.onDestroy();
     }
+
     private fun getYouTubeId(youTubeUrl: String): String? {
         val pattern = "(?<=youtu.be/|watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*"
         val compiledPattern = Pattern.compile(pattern)
         val matcher = compiledPattern.matcher(youTubeUrl)
-         if (matcher.find()) {
-           videoId= matcher.group()
+        if (matcher.find()) {
+            videoId = matcher.group()
 
         }
         Log.e("videoId", videoId.toString())
@@ -134,35 +131,38 @@ class YouTubeActivity: YouTubeBaseActivity() {
     }
 
 
+    private fun playVideo() {
+        binding.youtubePlayer.initialize(
+            getString(R.string.api_key),
+            object : YouTubePlayer.OnInitializedListener {
+                // Implement two methods by clicking on red error bulb
+                // inside onInitializationSuccess method
+                // add the video link or the
+                // playlist link that you want to play
+                // In here we also handle the play and pause functionality
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider?,
+                    player: YouTubePlayer?,
+                    p2: Boolean,
+                ) {
+                    Toast.makeText(AppClass.appContext, "Video player Success", Toast.LENGTH_SHORT)
+                        .show()
+                    //  player?.loadVideo(urlLink.toString())
+                    player?.loadVideo(videoId)
+                    player?.play()
+                }
 
-    private fun playVideo(){
-        binding.youtubePlayer.initialize(getString(R.string.api_key), object : YouTubePlayer.OnInitializedListener{
-            // Implement two methods by clicking on red error bulb
-            // inside onInitializationSuccess method
-            // add the video link or the
-            // playlist link that you want to play
-            // In here we also handle the play and pause functionality
-            override fun onInitializationSuccess(
-                provider: YouTubePlayer.Provider?,
-                player: YouTubePlayer?,
-                p2: Boolean,
-            ) {
-                Toast.makeText(AppClass.appContext , "Video player Success" , Toast.LENGTH_SHORT).show()
-                //  player?.loadVideo(urlLink.toString())
-                player?.loadVideo(videoId)
-                player?.play()
-            }
-
-            // Inside onInitializationFailure
-            // implement the failure functionality
-            // Here we will show toast
-            override fun onInitializationFailure(
-                p0: YouTubePlayer.Provider?,
-                p1: YouTubeInitializationResult?,
-            ) {
-                Toast.makeText(AppClass.appContext , "Video player Failed" , Toast.LENGTH_SHORT).show()
-            }
-        })
+                // Inside onInitializationFailure
+                // implement the failure functionality
+                // Here we will show toast
+                override fun onInitializationFailure(
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubeInitializationResult?,
+                ) {
+                    Toast.makeText(AppClass.appContext, "Video player Failed", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
 
     }
 
